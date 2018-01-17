cbbb275ea400cba750c65501a31eec6c53979d53
Toby Harris <operator@tobyz.net>
2018-01-17 12:34:22 +0000

    setup: Python 2,3 clarification

--- setup.py
+++ setup.py
@@ -41,8 +41,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3', # only tested for folk_rnn.py
+        'Programming Language :: Python :: 2.7', # training and generating
+        'Programming Language :: Python :: 3', # generating only
     ],
 
     # What does your project relate to?
